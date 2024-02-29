d8d0d44324b285b8301a2a3ec72f78f53037c8f2
Ashwini <ashwiniyh25@gmail.com>
2024-02-29 14:16:34 +0530

    test 4

--- index.php
+++ index.php
@@ -21,7 +21,7 @@ error_reporting(0);
         <div class="inner-header">
             <div class="logo">
                 <p>
-                    Welcome to KarnatakaBank
+                    Welcome to KarnatakaBank ATM
                 </p>
             </div>
         </div>
